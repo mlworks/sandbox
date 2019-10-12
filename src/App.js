739c89b22cc7b93f420904ba1340af93ab414325
Mike Lee <mlworks@gmail.com>
2019-10-12 13:27:13 -0700

    React router setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from 'react'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 
-const App = () => <div>WIP</div>
+// Pages
+import Home from './pages/home'
+
+const App = () => (
+  <Router>
+    <Link to="/">Home</Link>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>
+)
 
 export default App
