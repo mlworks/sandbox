cdce6f0294905bd367318240b6eef450df454705
Mike Lee <mlworks@gmail.com>
2019-10-13 01:09:15 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'normalize.css'
 import './index.css'
-import App from './_app'
+import App from './app'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
