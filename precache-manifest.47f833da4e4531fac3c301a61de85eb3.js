9c376cb8cc7c73501c035e73f7fa0060e39cb006
Mike Lee <mlworks@gmail.com>
2019-10-21 19:43:48 -0700

    Updates

---
+++ precache-manifest.47f833da4e4531fac3c301a61de85eb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58ad4d5926045e7b7273b78ac1524160",
+    "url": "/sandbox/index.html"
+  },
+  {
+    "revision": "070df0de2e4d1cf41aa4",
+    "url": "/sandbox/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "d5c1983b493d37e79390",
+    "url": "/sandbox/static/css/main.94d2bd32.chunk.css"
+  },
+  {
+    "revision": "070df0de2e4d1cf41aa4",
+    "url": "/sandbox/static/js/2.0ccdbf1a.chunk.js"
+  },
+  {
+    "revision": "d5c1983b493d37e79390",
+    "url": "/sandbox/static/js/main.fdf56e0d.chunk.js"
+  },
+  {
+    "revision": "8d8abef6144c6afa2b10",
+    "url": "/sandbox/static/js/runtime-main.bab1ad38.js"
+  }
+]);
