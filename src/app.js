7ea8481a69e146936bbc975c3b510f302c0dfb80
Mike Lee <mlworks@gmail.com>
2019-10-25 00:42:36 -0700

    Remove default element focus styles when using mouse interactions

--- src/app.js
+++ src/app.js
@@ -12,31 +12,48 @@ import Tabs from 'pages/tabs'
 import AppHeader from 'components/app-header'
 import Spacing from 'components/spacing'
 
-const App = () => (
-  <Router>
-    <AppHeader />
-    <main>
-      <Spacing className="app-contents" margin="0 auto" padding="xlg lg">
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/accordion">
-            <Accordion />
-          </Route>
-          <Route path="/form">
-            <Form />
-          </Route>
-          <Route path="/modal">
-            <ModalPage />
-          </Route>
-          <Route path="/tabs">
-            <Tabs />
-          </Route>
-        </Switch>
-      </Spacing>
-    </main>
-  </Router>
-)
+class App extends React.Component {
+  componentDidMount() {
+    window.addEventListener('mousedown', this.setMouseIntent)
+    window.addEventListener('keydown', this.removeMouseIntent)
+  }
+
+  setMouseIntent = () => document.body.classList.add('mouse-intent')
+
+  removeMouseIntent = event => {
+    if (event.keyCode === 9) {
+      document.body.classList.remove('mouse-intent')
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <AppHeader />
+        <main>
+          <Spacing className="app-contents" margin="0 auto" padding="xlg lg">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/accordion">
+                <Accordion />
+              </Route>
+              <Route path="/form">
+                <Form />
+              </Route>
+              <Route path="/modal">
+                <ModalPage />
+              </Route>
+              <Route path="/tabs">
+                <Tabs />
+              </Route>
+            </Switch>
+          </Spacing>
+        </main>
+      </Router>
+    )
+  }
+}
 
 export default App
--- src/components/app-header/app-header-sc.js
+++ src/components/app-header/app-header-sc.js
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 
 // Components
 import Button from 'components/button'
-import FlexBox from 'components/flex-box'
 import FlexItem from 'components/flex-item'
 
 // Constants
