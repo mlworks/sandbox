12d2fd5aaf6b28e5a0f42ae75ff5b5b65f0e173e
Mike Lee <mlworks@gmail.com>
2019-10-25 01:01:44 -0700

    Add hover animations to tab items

--- src/components/tabs/tab-button-sc.js
+++ src/components/tabs/tab-button-sc.js
@@ -7,9 +7,31 @@ import Button from 'components/button'
 import {colors} from 'constants/styles'
 
 const TabButtonSC = styled(Button)`
-  border-bottom: 2px solid
-    ${props => (props['aria-selected'] ? colors.accentRed : 'transparent')};
+  position: relative;
+
   font-weight: ${props => (props['aria-selected'] ? 'bold' : 'normal')};
+
+  &:hover {
+    background: none;
+
+    &::after {
+      transform: scale(1);
+    }
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: block;
+    height: 2px;
+    background: ${colors.accentRed};
+    transform: scale(${props => (props['aria-selected'] ? 1 : 0)});
+    transform-origin: bottom center;
+    transition: 0.1s cubic-bezier(0.25, 0.7, 0.01, 0.8) transform;
+  }
 `
 
 export default TabButtonSC
