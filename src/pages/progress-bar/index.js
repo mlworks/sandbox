89b333b3d57847c89f5967131d1cc5302b62f671
Mike Lee <mlworks@gmail.com>
2019-10-28 19:46:16 -0700

    WIP timer demo

--- src/pages/progress-bar/index.js
+++ src/pages/progress-bar/index.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 // Components
 import Card from 'components/card'
 import ProgressBar from 'components/progress-bar'
 
-const ProgressBarPage = () => (
-  <Card
-    style={{background: 'linear-gradient(-45deg, #031d4a 0%, #0d26f1 100%)'}}
-  >
-    <ProgressBar currentValue={40} />
-  </Card>
-)
+const getProgressValue = value => {
+  const num = Math.max(0, Math.min(value + Math.random() * 30, 100))
+  return Math.round(num)
+}
+
+const timer = update => setTimeout(update, Math.random() * 2500)
+
+const ProgressBarPage = () => {
+  const [currentValue, setCurrentValue] = useState(0)
+
+  // Set timer to simulate progress updates
+  const update = () => setCurrentValue(getProgressValue(currentValue))
+  useEffect(() => {
+    timer(update)
+  }, [currentValue])
+
+  return (
+    <Card
+      style={{background: 'linear-gradient(-45deg, #031d4a 0%, #0d26f1 100%)'}}
+    >
+      <ProgressBar currentValue={currentValue} />
+    </Card>
+  )
+}
 
 export default ProgressBarPage
